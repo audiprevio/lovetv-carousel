3f95470aeda060a30064082d6d0e389891d10a4a
Audi <audi.previo98@gmail.com>
2023-11-09 12:07:41 +0700

    fixed the height issue

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import MovieTitle from '../components/MovieTitle';
 import EpisodeSwiper from '../components/EpisodeSwiper';
 
 const MainPage = () => {
+  useEffect(() => {
+    const updateVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    window.addEventListener('load', updateVH);
+    window.addEventListener('resize', updateVH);
+
+    return () => {
+      window.removeEventListener('load', updateVH);
+      window.removeEventListener('resize', updateVH);
+    };
+  }, []);
+
   return (
     <div className='relative'>
       <Navbar className="z-[51] absolute" />
